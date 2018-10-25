ab1bf4d2fbf3406b170c27ee26961743feff0b50
Paul J. Davis <paul.joseph.davis@gmail.com>
2018-10-25 16:13:48 -0500

    Implement `fabric_util:open_cluster_db`
    This allows for more fine grained use of couch_db:clustered_db as well
    as chagnes the name to something more appropriate than `fake_db`.

--- src/fabric/src/fabric_doc_update.erl
+++ src/fabric/src/fabric_doc_update.erl
@@ -104,12 +104,12 @@ handle_message({request_entity_too_large, Entity}, _, _) ->
 before_doc_update(DbName, Docs, Opts) ->
     case {fabric_util:is_replicator_db(DbName), fabric_util:is_users_db(DbName)} of
         {true, _} ->
-            %% fake db is expensive to create so we only do it if we have to
-            Db = fabric_util:fake_db(DbName, Opts),
+            %% cluster db is expensive to create so we only do it if we have to
+            Db = fabric_util:open_cluster_db(DbName, Opts),
             [couch_replicator_manager:before_doc_update(Doc, Db) || Doc <- Docs];
         {_, true} ->
-            %% fake db is expensive to create so we only do it if we have to
-            Db = fabric_util:fake_db(DbName, Opts),
+            %% cluster db is expensive to create so we only do it if we have to
+            Db = fabric_util:open_cluster_db(DbName, Opts),
             [couch_users_db:before_doc_update(Doc, Db) || Doc <- Docs];
         _ ->
             Docs
